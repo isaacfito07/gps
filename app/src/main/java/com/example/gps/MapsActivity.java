570abc175dd7233e72b0f86f95cbcaf83b545623
Isaac <isaacfito07@gmail.com>
2022-09-27 11:21:48 -0600

    Prueba en equipo de escuela

--- app/src/main/java/com/example/gps/MapsActivity.java
+++ app/src/main/java/com/example/gps/MapsActivity.java
@@ -23,7 +23,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        //Prueba de GIT
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
